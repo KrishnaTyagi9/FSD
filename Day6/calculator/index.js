42f4259f4466dbd5c54a663a50a00f1863decd1a
KrishnaTyagi9 <krishnatyagi6010@gmail.com>
2025-10-14 10:25:04 +0530

    Calculator exercise

---
+++ Day6/calculator/index.js
@@ -0,0 +1,19 @@
+const num1=document.getElementById("num1");
+const num2=document.getElementById("num2");
+const output=document.getElementById("output");
+function add(){
+    console.log(num1.value+num2.value);
+    console.log("add");
+}
+function sub(){
+    console.log(num1.value-num2.value);
+    console.log("sub");
+}
+function mul(){
+    console.log(num1.value*num2.value);
+    console.log("mul");
+}
+function divi(){
+    console.log(num1.value/num2.value);
+    console.log("div");
+}
